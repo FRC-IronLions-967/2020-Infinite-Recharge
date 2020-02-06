c23946aab798bfea6a44adbd01f64a279b2c2b97
nstark03 <46936668+nstark03@users.noreply.github.com>
2020-02-06 04:36:32 -0600

    Added base function for calculating turning stuff

---
+++ 2020InfiniteRecharge/src/main/java/frc/robot/utils/values/TurningFunction.java
@@ -0,0 +1,33 @@
+package frc.robot.utils.values;
+
+public class TurningFunction {
+    private double deadband, minPower, aimPower, highCutoff;
+    private double table[] = new double[100];
+    public TurningFunction(double deadband, double minPower, double aimPower, double highCutoff) {
+        this.deadband = deadband;
+        this.minPower = minPower;
+        this.aimPower = aimPower;
+        this.highCutoff = highCutoff;
+
+        int tableIndex = 0;
+        while(tableIndex < deadband * 100) {
+            table[tableIndex] = 0;
+            tableIndex++;
+        }
+        double aimSlope = (aimPower - minPower)/(highCutoff - deadband);
+        while(tableIndex < highCutoff * 100) {
+            table[tableIndex] = (aimSlope * (((double) tableIndex / 100) - deadband)) + minPower;
+            tableIndex++;
+        }
+        double driveSlope = (1.0 - aimPower)/(1.0 - highCutoff);
+        while(tableIndex < 100) {
+            table[tableIndex] = (driveSlope * (((double) tableIndex / 100) - highCutoff)) + aimPower;
+            tableIndex++;
+        }
+    }
+
+    public double[] getTable() {
+        return table;
+    }
+
+}
