18213edbe04c99c862d89a6f9e4b396754e4c80c
nstark03 <46936668+nstark03@users.noreply.github.com>
2020-04-28 10:32:26 -0500

    Added some cpp and removed the state project

--- 2020InfiniteRecharge/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ 2020InfiniteRecharge/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -9,7 +9,6 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Subsystem;
