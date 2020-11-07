c661348adf6551b81300bf7f3f54e51853a079c5
nastark123 <43659552+nastark123@users.noreply.github.com>
2020-11-07 09:22:43 -0600

    Removed unused imports

--- 2020InfiniteRecharge/src/main/java/frc/robot/IO.java
+++ 2020InfiniteRecharge/src/main/java/frc/robot/IO.java
@@ -1,8 +1,5 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj2.command.button.POVButton;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.*;
 import frc.robot.utils.controls.XBoxController;
 
